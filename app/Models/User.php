d179ebcc0f4529b908548949964eb4913e8b8229
Mohammadreza Kadivar <kadivar@outlook.com>
2022-03-11 17:36:49 +0330

    Add admin factory to user model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Database\Factories\AdminFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Modules\Core\Database\Factories\NormalUserFactory;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return AdminFactory
+     */
+    protected static function admin_factory(): AdminFactory
+    {
+        return AdminFactory::new();
+    }
 }

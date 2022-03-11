aef9d9c293dad423ec78dd34b8c0cb6b966c3d24
Mohammadreza Kadivar <kadivar@outlook.com>
2022-03-11 19:02:35 +0330

    Add database migrations.

---
+++ database/migrations/2022_03_11_152825_create_cards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('box_id')->unsigned();
+            $table->foreign('box_id')
+                ->references('id')
+                ->on('boxes');
+            $table->longText('question');
+            $table->longText('answer');
+            $table->longText('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+};

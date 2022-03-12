acd41033aa04ceb3c998f4ce5f77145f417e2037
Mohammadreza Kadivar <kadivar@outlook.com>
2022-03-12 15:05:25 +0330

    Flashcard commands unit tests scaffold.

---
+++ tests/Unit/FlashcardConsoleTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FlashcardConsoleTest extends TestCase
+{
+    /**
+     * Test Create card command
+     *
+     * @return void
+     */
+    public function test_create_card_command()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test List cards command
+     *
+     * @return void
+     */
+    public function test_list_cards_command()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test Practice command
+     *
+     * @return void
+     */
+    public function test_practice_command()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test Stats command
+     *
+     * @return void
+     */
+    public function test_stats_command()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test Reset command
+     *
+     * @return void
+     */
+    public function test_reset_command()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * Test Exit command
+     *
+     * @return void
+     */
+    public function test_exit_command()
+    {
+        $this->assertTrue(false);
+    }
+}

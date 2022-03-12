c01c0724bf408c6bd629646bd65b31b286a15722
Mohammadreza Kadivar <kadivar@outlook.com>
2022-03-12 16:50:16 +0330

    add basic test for create card command.

--- tests/Unit/FlashcardConsoleTest.php
+++ tests/Unit/FlashcardConsoleTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Card;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Console\Commands;
 
 class FlashcardConsoleTest extends TestCase
 {
@@ -13,7 +16,12 @@ class FlashcardConsoleTest extends TestCase
      */
     public function test_create_card_command()
     {
-        $this->assertTrue(false);
+        $this->artisan('flashcard:interactive')
+            ->expectsQuestion('Please choose one of listed options (Just number of Item)', 1)
+            ->expectsQuestion('Enter the question', 'This is a unit test generation question?')
+            ->expectsQuestion('Enter the answer', 'This is a unit test generation answer.')
+            ->expectsOutput('New card successfully created.')
+            ->assertExitCode(0);
     }
 
     /**
@@ -23,7 +31,7 @@ class FlashcardConsoleTest extends TestCase
      */
     public function test_list_cards_command()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 
     /**
@@ -31,9 +39,39 @@ class FlashcardConsoleTest extends TestCase
      *
      * @return void
      */
-    public function test_practice_command()
+    public function test_success_new_practice_command()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test Practice command
+     *
+     * @return void
+     */
+    public function test_failed_new_practice_command()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test Practice command
+     *
+     * @return void
+     */
+    public function test_success_old_practice_command()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test Practice command
+     *
+     * @return void
+     */
+    public function test_failed_old_practice_command()
+    {
+        $this->assertTrue(true);
     }
 
     /**
@@ -43,7 +81,7 @@ class FlashcardConsoleTest extends TestCase
      */
     public function test_stats_command()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 
     /**
@@ -53,7 +91,7 @@ class FlashcardConsoleTest extends TestCase
      */
     public function test_reset_command()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 
     /**
@@ -63,6 +101,6 @@ class FlashcardConsoleTest extends TestCase
      */
     public function test_exit_command()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 }

ed2cafcd82e4a71d462c1f512c41bf7834e2a223
Mohammadreza Kadivar <kadivar@outlook.com>
2022-04-06 02:24:14 +0430

    add some assertion for stat, history and list card.

--- tests/Unit/FlashcardConsoleTest.php
+++ tests/Unit/FlashcardConsoleTest.php
@@ -31,7 +31,7 @@ class FlashcardConsoleTest extends TestCase
      *
      * @return void
      */
-    public function setUp():void
+    public function setUp(): void
     {
         parent::setUp();
         $this->flashcard = $this->mock(Commands\Flashcard::class);
@@ -42,7 +42,7 @@ class FlashcardConsoleTest extends TestCase
      *
      * @return void
      */
-    public function tearDown():void
+    public function tearDown(): void
     {
 
     }
@@ -52,7 +52,72 @@ class FlashcardConsoleTest extends TestCase
      *
      * @return void
      */
-    public function testGetStatClass():void
+    public function testGetStatClass(): void
+    {
+        $stat = new Commands\Classes\Flashcard\Stat($this->flashcard);
+        $result = $stat->get();
+        $this->assertIsArray($result);
+        $this->assertCount(1, $result);
+        $this->assertArrayHasKey('total', $result[0]);
+        $this->assertArrayHasKey('total_answers', $result[0]);
+        $this->assertArrayHasKey('correct_answers', $result[0]);
+        $this->assertIsNumeric($result[0]['total']);
+        $this->assertIsString($result[0]['total_answers']);
+        $this->assertIsString($result[0]['correct_answers']);
+    }
+
+    /**
+     * Test
+     *
+     * @return void
+     */
+    public function testGetHistoryClass(): void
+    {
+        $history = new Commands\Classes\Flashcard\History($this->flashcard);
+        $result = $history->get();
+        $last_item_index = count($result) - 1;
+        $this->assertIsArray($result);
+        $this->assertGreaterThan(0, count($result));
+        $this->assertArrayHasKey('id', $result[0]);
+        $this->assertArrayHasKey('question', $result[0]);
+        $this->assertArrayHasKey('status', $result[0]);
+        $this->assertIsNumeric($result[0]['id']);
+        $this->assertIsString($result[0]['question']);
+        $this->assertIsString($result[0]['status']);
+        $this->assertArrayHasKey(' ', $result[$last_item_index]);
+        $this->assertArrayHasKey('question', $result[$last_item_index]);
+        $this->assertArrayHasKey('status', $result[$last_item_index]);
+        $this->assertSame($result[$last_item_index][' '], ' ');
+        $this->assertSame($result[$last_item_index]['question'], '<fg=green> % of completion</>');
+        $this->assertIsString($result[$last_item_index][' ']);
+        $this->assertIsString($result[$last_item_index]['status']);
+    }
+
+    /**
+     * Test
+     *
+     * @return void
+     */
+    public function testListCardClass(): void
+    {
+        $card = new Commands\Classes\Flashcard\Card($this->flashcard);
+        $result = $card->list();
+        $this->assertIsArray($result);
+        $this->assertGreaterThan(0, count($result));
+        $this->assertArrayHasKey('id', $result[0]);
+        $this->assertArrayHasKey('question', $result[0]);
+        $this->assertArrayHasKey('answer', $result[0]);
+        $this->assertIsNumeric($result[0]['id']);
+        $this->assertIsString($result[0]['question']);
+        $this->assertIsString($result[0]['answer']);
+    }
+
+    /**
+     * Test
+     *
+     * @return void
+     */
+    public function testCreateCardClass(): void
     {
         $this->assertTrue(true);
     }
@@ -62,7 +127,7 @@ class FlashcardConsoleTest extends TestCase
      *
      * @return void
      */
-    public function testGetHistoryClass():void
+    public function testInitPracticeClass(): void
     {
         $this->assertTrue(true);
     }
@@ -72,7 +137,7 @@ class FlashcardConsoleTest extends TestCase
      *
      * @return void
      */
-    public function testListCardClass():void
+    public function testPromptClass(): void
     {
         $this->assertTrue(true);
     }
@@ -82,37 +147,7 @@ class FlashcardConsoleTest extends TestCase
      *
      * @return void
      */
-    public function testCreateCardClass():void
-    {
-        $this->assertTrue(true);
-    }
-
-    /**
-     * Test
-     *
-     * @return void
-     */
-    public function testInitPracticeClass():void
-    {
-        $this->assertTrue(true);
-    }
-
-    /**
-     * Test
-     *
-     * @return void
-     */
-    public function testPromptClass():void
-    {
-        $this->assertTrue(true);
-    }
-
-    /**
-     * Test
-     *
-     * @return void
-     */
-    public function testResetClass():void
+    public function testResetClass(): void
     {
         $this->assertTrue(true);
     }
